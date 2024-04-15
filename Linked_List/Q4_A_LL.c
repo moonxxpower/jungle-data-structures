a004f8d70abc1e841cecb9274d76fc4abb47edff
moonxxpower <su551732@gmail.com>
2024-04-16 03:49:23 +0900

    Q4_A_LL 구현

--- Linked_List/Q4_A_LL.c
+++ Linked_List/Q4_A_LL.c
@@ -84,9 +84,31 @@ int main()
 
 //////////////////////////////////////////////////////////////////////////////////
 
+/* add your code here */
+/* Move all the even integers to the back of the linked list */
 void moveEvenItemsToBack(LinkedList *ll)
 {
-	/* add your code here */
+	int index = 0;
+
+	ListNode * cur;
+	cur = ll -> head;
+
+	ListNode * temp = NULL;
+
+	for (int i = 0; i < ll -> size; i ++) {
+		if (cur -> item % 2 == 0) {
+			temp = cur -> next;
+
+			insertNode(ll, ll -> size, cur -> item);
+			removeNode(ll, index);
+
+			cur = temp;
+		} 
+		else {
+			cur = cur -> next;
+			index++;
+		}
+	}
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
